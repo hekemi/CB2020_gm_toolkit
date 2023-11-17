05d93d8b0359ed9c33d8b6b67ef8eef562f79014
Xint87 <alexander.surov2018@yandex.ru>
2023-11-18 01:24:00 +0300

    Update GUI 0.1.py

--- GUI 0.1.py
+++ GUI 0.1.py
@@ -5,11 +5,24 @@ from tkinter import ttk
 
 root = Tk()     
 root.title("Приложение на Tkinter")     
-root.geometry("300x250")
+root.geometry("350x420")
 
 root.resizable(True, True)
- 
-label = Label(text="Hello World") 
-label.pack()    
- 
+
+label = Label(text="CyberPunk 2020 GM's toolkit v 0.1") 
+
+
+for c in range(3): root.columnconfigure(index=c, weight=1)
+for r in range(4): root.rowconfigure(index=r, weight=1)
+
+title = Label(text="Welcome to CyberPunk 2020 GM's toolkit v 0.1")
+title.grid(row=0, column=0,columnspan=3, ipadx=10, ipady=5, padx=0, pady=0)
+
+InventoryManager = ttk.Button(text="Инвентарь")
+InventoryManager.grid(row=1, column=0, ipadx=10, ipady=5, padx=0, pady=0)
+
+DiceRoller = ttk.Button(text="Роллер")
+DiceRoller.grid(row=1,column=1,ipadx=10, ipady=5, padx=0, pady=0)
+
+
 root.mainloop()
