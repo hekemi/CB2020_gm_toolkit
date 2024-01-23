65a7c5520b6ce5252445a9b3bc3d5e09e91b3a0b
hekemi <vaonmax@gmail.com>
2024-01-23 22:42:35 +0300

    Created an inventory_manager.py

---
+++ scripts/inventory_manager.py
@@ -0,0 +1,14 @@
+import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from data.database_metods import *
+
+# self.CharacSel = QtWidgets.QComboBox(self.centralwidget)
+# self.CharacSel.setGeometry(QtCore.QRect(10, 10, 151, 22))
+# self.CharacSel.setObjectName("CharacSel")
+# self.RoleLb = QtWidgets.QLabel(self.centralwidget)
+
+names = os.listdir('data\characters')
+actual_name = []
+for i in names:
+    actual_name.append(i[:-3])
